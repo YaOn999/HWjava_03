3e057b848c4eaf1136951b07e70d32690f75057e
YaOn999 <lumous999@gmail.com>
2023-04-03 18:54:35 +0300

    add task

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("string");
+        strings.add("40");
+        strings.add("-5");
+        strings.add("5.5");
+        strings.add("my_string");
+        removeEvenNumber(strings);
+        System.out.println(strings);
+        removeIntegers(strings);
+        System.out.println(strings); // [string, my_string]
+    }
+
+    static void removeEvenNumber(List<String> numbers) {
+        // TODO: 31.03.2023 Удалить все четные элементы из списка
+        for (int i = 0; i < numbers.size(); i++) {
+            try {
+                Integer num = Integer.parseInt(numbers.get(i));
+                if (num % 2 == 0) {
+                    numbers.remove(i);
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+    }
+
+    static void removeIntegers(List<String> strings) {
+        // TODO: 31.03.2023 Удалить строки, которые являются целыми числами
+        for (int i = 0; i < strings.size(); i++) {
+            try {
+                Integer num = Integer.parseInt(strings.get(i));
+                if (num % 1 == 0) {
+                    strings.remove(i);
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+    }
+}
